dffbcab74a136402603a179028963e57114cd0df
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-09-15 17:05:25 -0700

    fimo & motifMatcher methods both work

--- R/tfBindingSites.R
+++ R/tfBindingSites.R
@@ -3,6 +3,7 @@
 #' @import FimoClient
 #' @import RSQLite
 #' @import GenomicRanges
+#' @import trena
 #'
 #' @name tfBindingSites-class
 #' @rdname tfBindingSites-class
@@ -12,13 +13,19 @@
 .tfBindingSites <- setClass("tfBindingSites",
                        slots = c(
                           tf="character",
+                          genome="BSgenome",
                           fimoClient="FimoClientClass",
                           quiet="logical",
-                          state="environment"
+                          state="environment",
+                          db="SQLiteConnection"
                           ),
                        )
 
-
+#------------------------------------------------------------------------------------------------------------------------
+setGeneric('getChipSeqHits', signature='obj', function (obj, chromosome, min, max) standardGeneric ('getChipSeqHits'))
+setGeneric('getFimoHits',   signature='obj',  function (obj, tbl.regions) standardGeneric ('getFimoHits'))
+setGeneric('getMotifMatcherHits', signature='obj', function(obj, tbl.regions, pfms, matchThreshold)
+              standardGeneric('getMotifMatcherHits'))
 #------------------------------------------------------------------------------------------------------------------------
 #' Create a tfBindingSitesCtor object
 #'
@@ -36,16 +43,104 @@
 #'
 #' @export
 
-tfBindingSitesCtor <- function(tf, quiet=TRUE)
+tfBindingSitesCtor <- function(tf, genome, chipseq.db.file, fimoHost, fimoPort, quiet=TRUE)
 {
-   FIMO_HOST <- "localhost"
-   FIMO_PORT <- 5558
-   fimoClient <- FimoClient(FIMO_HOST, FIMO_PORT, quiet=quiet)
+   fimoClient <- FimoClient(fimoHost, fimoPort, quiet=quiet)
 
+   if(!quiet)
+      printf("connecting to SQLite ChIP-seq db.file '%s'", chipseq.db.file)
+
+   db <- dbConnect(dbDriver("SQLite"), chipseq.db.file)
    state <- new.env(parent=emptyenv())
-   obj <- .tfBindingSites(tf=tf, fimoClient=fimoClient, quiet=quiet, state=state)
+
+   obj <- .tfBindingSites(tf=tf, genome=genome, fimoClient=fimoClient, quiet=quiet, state=state, db=db)
 
    obj
 
 } # tfBindingSites ctor
+#------------------------------------------------------------------------------------------------------------------------
+#' get a table of ChIP-seq hits of the specified tf, in the specified region
+#'
+#' @rdname getChipSeqHits
+#' @aliases getChipSeqHits
+#'
+#' @param obj An object of class tfBindingSites
+#' @param chromosome character
+#' @param min numeric
+#' @param max numeric
+#'
+#' @return A data.frame
+#'
+#' @export
+
+setMethod('getChipSeqHits', 'tfBindingSites',
+
+     function (obj, chromosome, min, max){
+        query <- sprintf("select * from chipseq where chr='%s' and start >= %d and end <= %d and tf = '%s'",
+                          chromosome, min, max, obj@tf)
+        tbl <- dbGetQuery(obj@db, query)
+        tbl
+        })
+
+#------------------------------------------------------------------------------------------------------------------------
+#' get a table of FIMO hits in the regions specified in the incoming data.frame
+#'
+#' @rdname getFimoHits
+#' @aliases getFimoHits
+#'
+#' @param obj An object of class tfBindingSites
+#' @param tbl a data.frame with (at least) chr, start, end columns
+#'
+#' @return a data.frame
+#'
+#' @export
+
+setMethod('getFimoHits', 'tfBindingSites',
+
+     function (obj, tbl.regions){
+        stopifnot(all(c("chr", "start", "end") %in% colnames(tbl.regions)))
+        seqs <- with(tbl.regions, as.list(as.character(getSeq(obj@genome, chr, start, end))))
+        names(seqs) <- sprintf("region.%02d", seq_len(nrow(tbl.regions)))
+        tbl <- requestMatch(obj@fimoClient, seqs, 10e-3)
+        if(nrow(tbl) > 0){
+           tbl$chr <- tbl.regions$chr[1]
+           tbl$pvalScore <- -log10(tbl$p.value)
+           for(r in 1:nrow(tbl.regions)){
+              chrom.start <- tbl.regions[r, "start"]
+              rows.oi <- grep(sprintf("region.%02d", r), tbl$sequence.name)
+              printf(" %d rows.oi for row %d", rows.oi, r)
+              if(length(rows.oi) > 0){
+                 tbl$start[rows.oi] <- tbl$start[rows.oi] + chrom.start;
+                 tbl$stop[rows.oi] <- tbl$stop[rows.oi] + chrom.start;
+                 } # ir rows.oi
+              } # for r
+           tbl <- tbl[, c("chr", "start", "stop", "pvalScore", "strand", "motif", "sequence.name", "matched.sequence")]
+           }
+        tbl
+        })
+
+#------------------------------------------------------------------------------------------------------------------------
+#' get a table of bioconductor (MotifMatcher) hits in the regions specified in the incoming data.frame
+#'
+#' @rdname getMotifMatcherHits
+#' @aliases getMotifMatcherHits
+#'
+#' @param obj An object of class tfBindingSites
+#' @param tbl.regions a data.frame with (at least) chr, start, end columns
+#' @parm pfms a list of position frequency matrices, from e.g. MotifDb
+#'
+#' @return a data.frame
+#'
+#' @export
+
+setMethod('getMotifMatcherHits', 'tfBindingSites',
+
+   function(obj, tbl.regions, pfms, matchThreshold=0.7){
+      if(colnames(tbl.regions)[1] == "chr")
+         colnames(tbl.regions)[1] <- "chrom"
+      mm <- MotifMatcher("hg38", as.list(pfms))
+      tbl <- findMatchesByChromosomalRegion(mm, tbl.regions, pwmMatchMinimumAsPercentage=as.integer(100 * matchThreshold))
+      tbl
+      })
+
 #------------------------------------------------------------------------------------------------------------------------
--- inst/unitTests/test_tfBindingSites.R
+++ inst/unitTests/test_tfBindingSites.R
@@ -1,17 +1,93 @@
 library(tfBindingSites)
 library(RUnit)
+library(BSgenome.Hsapiens.UCSC.hg38)
 #------------------------------------------------------------------------------------------------------------------------
 runTests <- function()
 {
    test_constructor()
+   test_getChipSeqHits()
+   test_runFIMO()
+   test_motifMatcher()
 
 } # runTests
 #------------------------------------------------------------------------------------------------------------------------
 test_constructor <- function()
 {
    printf("--- test_constructor")
-   tfbs <- tfBindingSitesCtor("CREM")
+   tfbs <- tfBindingSitesCtor("CREM",
+                              genome=BSgenome.Hsapiens.UCSC.hg38,
+                              chipseq.db.file <- "~/s/data/public/human/remap-2018/remap-all.sqlite",
+                              fimoHost="localHost",
+                              fimoPort=5558,
+                              quiet=TRUE)
+
    checkEquals(is(tfbs), "tfBindingSites")
 
 } # test_constructor
+#------------------------------------------------------------------------------------------------------------------------
+test_getChipSeqHits <- function()
+{
+   printf("--- test_getChipSeqHits")
+
+   tf <- "CREM"
+   tfbs <- tfBindingSitesCtor(tf,
+                              genome=BSgenome.Hsapiens.UCSC.hg38,
+                              chipseq.db.file= "~/s/data/public/human/remap-2018/remap-all.sqlite",
+                              fimoHost="localHost",
+                              fimoPort=5558,
+                              quiet=TRUE)
+
+   tbl.cs.crem <- getChipSeqHits(tfbs, "chr1", 167517115, 167525862)
+   checkEquals(dim(tbl.cs.crem), c(2, 7))
+   checkEquals(tbl.cs.crem$tf,  c(tf, tf))
+
+    # save(tbl.cs.crem, file="../extdata/tbl.ts.crem.2.chr1.167517115.167525862.RData")
+
+} # test_getChipSeqHits
+#------------------------------------------------------------------------------------------------------------------------
+test_runFIMO <- function()
+{
+   printf("--- test_runFIMO")
+
+   tf <- "CREM"
+   tfbs <- tfBindingSitesCtor(tf,
+                              genome=BSgenome.Hsapiens.UCSC.hg38,
+                              chipseq.db.file="~/s/data/public/human/remap-2018/remap-all.sqlite",
+                              fimoHost="localHost",
+                              fimoPort=5558,
+                              quiet=FALSE)
+
+   load(system.file(package="tfBindingSites", "extdata", "tbl.ts.crem.2.chr1.167517115.167525862.RData"))
+   tbl.cs <- getFimoHits(tfbs, tbl.cs.crem)
+   #checkEquals(dim(tbl.cs), c(2, 7))
+   #checkEquals(tbl.ts$tf <- tf)
+
+} # test_runFIMO
+#------------------------------------------------------------------------------------------------------------------------
+test_motifMatcher <- function()
+{
+   printf("--- test_runMotifMatcher")
+
+   tf <- "CREM"
+   tfbs <- tfBindingSitesCtor(tf,
+                              genome=BSgenome.Hsapiens.UCSC.hg38,
+                              chipseq.db.file="~/s/data/public/human/remap-2018/remap-all.sqlite",
+                              fimoHost="localHost",
+                              fimoPort=5558,
+                              quiet=FALSE)
+
+   load(system.file(package="tfBindingSites", "extdata", "tbl.ts.crem.2.chr1.167517115.167525862.RData"))
+
+   pfms <- MotifDb["Hsapiens-HOCOMOCOv10-CREM_HUMAN.H10MO.C"]
+
+     # downstream constraints on pfms
+   # MotifMatcher, .matchPwmForwardAndReverse(sequence, pfms[[i]], names(pfms)[i], min.match.percentage, quiet)
+   # pfms[[i]] must be strictly a numeric matrix
+   # name(pfms)[i] must be just a string
+
+
+   tbl.cs <- getMotifMatcherHits(tfbs, tbl.cs.crem, pfms, matchThreshold=0.7)
+   checkTrue(nrow(tbl.cs) >= 4)
+
+} # test_motifMatcher
 #------------------------------------------------------------------------------------------------------------------------
